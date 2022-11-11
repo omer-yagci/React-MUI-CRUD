b45f68078183ed17139924b339dcdbcf5d1011bd
khan <omeryagci3295@gmail.com>
2022-11-11 21:23:19 +0300

    header and sidemenu added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import SideMenu from "./components/SideMenu";
+import { makeStyles } from "@material-ui/core";
+import Header from "./components/Header";
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: "320px",
+    width: "100%",
+  },
+});
 
 function App() {
-  return <div className="App"></div>;
+  const clases = useStyles();
+  return (
+    <>
+      <SideMenu />
+      <div className={clases.appMain}>
+        <Header />
+      </div>
+    </>
+  );
 }
 
 export default App;
