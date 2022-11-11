0e501459b84f5ef8ef07ca83bc11e13a4bc22eaa
khan <omeryagci3295@gmail.com>
2022-11-11 22:24:40 +0300

    header done ?

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,14 +10,39 @@ import {
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import SearchIcon from "@material-ui/icons/Search";
+
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#fff",
+  },
+  searchInput: {
+    opacity: "0.6",
+    padding: `0px ${theme.spacing(1)}px`,
+    fontSize: "0.8rem",
+    "&:hover": {
+      backgroundColor: "#f2f2f2",
+    },
+    "& .MuiSvgIcon-root": {
+      marginRight: theme.spacing(1),
+    },
+  },
+}));
 
 const Header = () => {
+  const clases = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={clases.root}>
       <Toolbar>
         <Grid container>
           <Grid item>
-            <InputBase />
+            <InputBase
+              placeholder="Text Here"
+              className={clases.searchInput}
+              startAdornment={<SearchIcon fontSize="small" />}
+            />
           </Grid>
           <Grid item sm></Grid>
           <Grid item>
@@ -32,7 +57,7 @@ const Header = () => {
               </Badge>
             </IconButton>
             <IconButton>
-              <PowerSettingsNewIcon />
+              <PowerSettingsNewIcon fontSize="small" />
             </IconButton>
           </Grid>
         </Grid>
